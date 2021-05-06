b758de027a4766733b9b2ad2c69e423cd1b3b2be
talahajeer <tala.hajeer@gmail.com>
2021-05-06 19:03:00 +0300

    code 05

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,52 @@
-class LinkedList:
-    """
-    Put docstring here
-    """
+class Node:
+  def __init__(self, data=None):
+      self.data = data
+      self.next = None
+  
+  def __str__(self):
+      return f"{self.data}"
+   
 
-    def __init__(self):
-        # initialization here
-        pass
 
-    def some_method(self):
-        # method body here
-        pass
+class Linked_list:
+  def __init__(self):
+    self.head = None
+
+  def insert (self, data=None):
+    new_node = Node(data)
+    if self.head :
+      new_node.next = self.head
+    self.head = new_node
+
+  def includes(self,value):
+    current = self.head 
+    is_include = False
+    while (current):
+        if current.data == value :
+            is_include = True
+        current = current.next
+    return is_include
+      
+
+  def __str__(self):
+    """ Returns a string representaiton of the linked list
+        1 -> 3 -> 4 -> Null
+    """
+    output = ""
+
+    current = self.head
+    while current:
+      output += "{%s} -> " %(current.data,)
+      current = current.next
+    output += " NULL"
+    return output
+  
+
+
+if __name__ == "__main__":
+  linked = Linked_list()
+  linked.insert("Muhannad")
+  linked.insert("Manar")
+  linked.insert(10)
+#   print(linked)
+#   print(linked.includes(55),linked.includes("Manar"),linked.includes(5))
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,21 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import *
+import pytest
+
+def test_insert(list_test):
+    excpected = "{10} -> {Manar} -> {Muhannad} ->  NULL"
+    actual = f"{list_test}"
+    assert excpected == actual
+
+def test_includes(list_test):
+    actual = [list_test.includes(55),list_test.includes("Manar"),list_test.includes(5)]
+    excpected = [False, True , False]
+    assert excpected == actual
 
 
-def test_import():
-    assert LinkedList
+@pytest.fixture
+def list_test():
+    linked = Linked_list()
+    linked.insert("Muhannad")
+    linked.insert("Manar")
+    linked.insert(10)
+    return linked
