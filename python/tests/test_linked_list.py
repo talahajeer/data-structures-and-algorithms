43b0978f49b344714ffc5d1b78658adaab6c5cb1
talahajeer <tala.hajeer@gmail.com>
2021-05-17 23:14:00 +0300

    trying

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,38 +1,53 @@
 from linked_list.linked_list import *
 import pytest
 
-def test_insert(list_test):
+def test_insert(list_test_insert):
     excpected = "{10} -> {Manar} -> {Muhannad} ->  NULL"
-    actual = f"{list_test}"
+    actual = f"{list_test_insert}"
     assert excpected == actual
 
-def test_includes(list_test):
-    actual = [list_test.includes(55),list_test.includes("Manar"),list_test.includes(5)]
+def test_includes(list_test_insert):
+    actual = [list_test_insert.includes(55),list_test_insert.includes("Manar"),list_test_insert.includes(5)]
     excpected = [False, True , False]
     assert excpected == actual
 
-def test_append(list_test):
+def test_append(list_test_append):
     excpected = "{10} -> {Manar} -> {Muhannad} -> {Adel} ->  NULL"
-    actual = f"{list_test}"
+    actual = f"{list_test_append}"
     assert excpected == actual
 
-def test_insertAfter(list_test):
-    actual = f"{list_test}"
+def test_insertAfter(list_test_insertAfter):
+    actual = f"{list_test_insertAfter}"
     excpected = "{10} -> {Tala} -> {Manar} -> {Muhannad} ->  NULL"
     assert excpected == actual
 
-def test_insertBefore(list_test):
-    actual = f"{list_test}"
+def test_insertBefore(list_test_insertBefore):
+    actual = f"{list_test_insertBefore}"
     excpected = "{10} -> {One} -> {Tala} -> {Manar} -> {Muhannad} ->  NULL"
     assert excpected == actual
 
 @pytest.fixture
-def list_test():
+def list_test_insert():
     linked = Linked_list()
     linked.insert("Muhannad")
     linked.insert("Manar")
     linked.insert(10)
-    linked.append("Adel")
-    linked.insertAfter(10,"Tala")
-    linked.insertBefore("Tala","One")
     return linked
+
+@pytest.fixture
+def list_test_append():
+    linked = Linked_list()
+    linked.append("Adel")  
+    return linked
+
+@pytest.fixture
+def list_test_insertAfter():
+    linked = Linked_list()
+    linked.insertAfter(10,"Tala")
+    return linked    
+
+@pytest.fixture
+def list_test_insertBefore():
+    linked = Linked_list()
+    linked.insertBefore("Tala","One")    
+    return linked      
