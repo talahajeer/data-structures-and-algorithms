900b8d30a594a41b614ae19da114237bc4cfd400
talahajeer <tala.hajeer@gmail.com>
2021-06-18 22:25:01 +0300

    merge_sort

--- python/challenges/merge_sort/merge_sort.py
+++ python/challenges/merge_sort/merge_sort.py
@@ -1,28 +1,37 @@
 def merge_sort(arr):
-    n = len(arr)
 
-    if n >1:
-        mid = n/2
-        left = arr[0:mid]
-        right = arr[mid:n]
-        merge_sort(left) 
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort(left)
         merge_sort(right)
-        merge(left,right,arr)      
+        merge(left, right, arr)
+    # print(arr)
+    return arr     
 
 def merge(left,right,arr):
     i = 0
     j = 0
     k = 0
-    while i< len(left) and j< len(right):
+    while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             arr[k] = left[i]
-            i = i+1
+            i = i + 1
         else:
             arr[k] = right[j]
             j = j + 1
-        k = k+1
+        k = k + 1
 
     if i == len(left):
-        pass
+        for item in right[j:]:
+            arr[k] = item
+            k += 1
     else:
-        pass
+        for item in left[i:]:
+            arr[k] = item
+            k += 1
+    # print('- merge ',arr)
+
+    arr = [8,4,23,42,16,15]
+    print(merge_sort(arr))
